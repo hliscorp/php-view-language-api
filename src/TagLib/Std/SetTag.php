a66589969345cef567b1d1b8f78fd92717753919
Lucian Gabriel Popescu <lucian@hliscorp.com>
2023-12-26 14:21:39 +0200

    bugfix

--- src/TagLib/Std/SetTag.php
+++ src/TagLib/Std/SetTag.php
@@ -23,15 +23,29 @@ class SetTag extends SystemTag implements StartTag
     public function parseStartTag(array $parameters=[]): string
     {
         $this->checkParameters($parameters, array("var"));
-        $left = '$'.$parameters['var'];
-        $right = "null";
-        if (isset($parameters['val'])) {
+        $output = '<?php $'.$parameters['var'].' = ';
+        if (isset($parameters["val"])) {
             if ($this->isExpression($parameters['val'])) {
-                $right = $this->parseExpression($parameters['val']);
+                $output .= $this->parseExpression($parameters['val']);
+            } else if ($this->isFunction($parameters['val'])) {
+                $output .= $parameters['val'];
             } else {
-                $right = "'".addslashes($parameters['val'])."'";
+                $output .= "'".addslashes($parameters['val'])."'";
             }
+        } else {
+            $output .= "null";
         }
-        return '<?php '.$left.' = '.$right.'; ?>';
+        return $output.'; ?>';
+    }
+
+    /**
+     * Checks if variable value is a function without expressions
+     * 
+     * @param string $expression
+     * @return bool
+     */
+    protected function isFunction($expression): bool
+    {
+        return (bool) preg_match("/^[a-zA-Z0-9_'\"]+\s*\(([^\)]+)\)$/", $expression);
     }
 }
